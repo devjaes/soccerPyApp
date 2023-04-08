7f0aec4bb73944d779f7b282854e810ccd4e5c8f
Jaesbyp <merajair17@gmail.com>
2023-04-08 00:19:09 -0500

    First implements

---
+++ README.md
@@ -0,0 +1,3 @@
+Soccer Python App
+
+This is a python app which let us watch live soccer matchs in our smartphones getting the live player of a web page
---
+++ main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.lang import Builder
+
+
+class Root(BoxLayout):
+    pass
+
+
+class MainApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    MainApp().run()
